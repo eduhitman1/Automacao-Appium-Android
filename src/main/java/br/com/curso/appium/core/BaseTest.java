56073159ae6403ac8050625823bda2d3f392ca56
Eduardo Henrique <edu.hitman01.eh@gmail.com>
2019-11-21 15:40:28 -0300

    Screenshots ao final dos testes

--- src/main/java/br/com/curso/appium/core/BaseTest.java
+++ src/main/java/br/com/curso/appium/core/BaseTest.java
@@ -1,7 +1,13 @@
 package br.com.curso.appium.core;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class BaseTest {
 	
@@ -12,7 +18,17 @@ public class BaseTest {
 	
 	@After
 	public void tearDown() {
+        gerarScreenShot();
 		DriverFactory.getDriver().resetApp();
-
+	}
+	
+	//PRINT DE TELA AO FINAL DE TEST
+	public void gerarScreenShot() {  
+	    try {
+	    	File imagem = ((TakesScreenshot) DriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(imagem, new File("target/screenshots/imagem.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
 	}
 }
